a030546a86f2e04ca86a0dcd9e9e97100b0b7807
izarte <inigozarate01@gmail.com>
2023-03-20 20:42:40 +0100

    Remove install package

--- jetson/ros2_ws/build/turtlebot3_example/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/catkin_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
--- jetson/ros2_ws/build/turtlebot3_simulations/colcon_command_prefix_build.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/catkin_ws/install/turtlebot3_description/share/turtlebot3_description/package.sh"
-. "/catkin_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
-. "/catkin_ws/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
-. "/catkin_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
