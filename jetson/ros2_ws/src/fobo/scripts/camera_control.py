f3ecd9ad5b4727e533083f418a50f9ea5df0a118
izarte <inigozarate01@gmail.com>
2023-03-25 13:55:24 +0100

    Add move control based on servo angle

--- jetson/ros2_ws/src/fobo/scripts/camera_control.py
+++ jetson/ros2_ws/src/fobo/scripts/camera_control.py
@@ -5,6 +5,7 @@ import Jetson.GPIO as GPIO
 import time
 
 from fobo.msg import CameraPose
+from fobo.msg import ServosPose
 
 
 class Servo():
@@ -16,6 +17,14 @@ class Servo():
 		GPIO.setup(self.direction, GPIO.OUT, initial=GPIO.HIGH)
 		self.angle = 90
 	
+	def incr_angle(self, direction):
+		angle = self.angle + direction * self.delta
+		if angle > 180:
+			angle = 180
+		elif angle < 0:
+			angle = 0
+		self.set_angle(angle)
+		
 	def set_angle(self, angle):
 		while(self.angle != angle):
 			if angle > self.angle:
@@ -32,6 +41,9 @@ class Servo():
 				GPIO.output(self.incr, 1)
 				time.sleep(10 / 1000)
 				self.angle -= self.delta
+	
+	def get_angle(self):
+		return (self.angle)
 
 class CameraControl(Node):
 	def __init__(self):
@@ -39,6 +51,12 @@ class CameraControl(Node):
 		GPIO.setmode(GPIO.BOARD)
 		self.x_motor = Servo(11, 12)
 		self.y_motor = Servo(15, 16)
+		self.servos_pose = ServosPose()
+		self.pub = self.create_publisher(
+			ServosPose,
+			'servos_pose',
+			10
+		)
 		self.sub = self.create_subscription(
 			CameraPose,
 			'camera_control',
@@ -46,10 +64,25 @@ class CameraControl(Node):
 			10
 		)
 		self.sub
+		self.range_pose_x = 10
+		self.range_pose_y = 10
 
 	def read_camera_pose(self, msg):
-		self.x_motor.set_angle(msg.x)
-		self.y_motor.set_angle(msg.y)
+		if msg.x > self.range_pose_x:
+			self.x_motor.incr_angle(1)
+		elif msg.x < -self.range_pose_x:
+			self.x_motor.incr_angle(-1)
+		
+		if msg.y > self.range_pose_y:
+			self.y_motor.incr_angle(1)
+		elif msg.y < -self.range_pose_y:
+			self.y_motor.incr_angle(-1)
+
+		self.servos_pose.servo_x = self.x_motor.get_angle()
+		self.servos_pose.servo_y = self.y_motor.get_angle()
+		self.pub.publish(self.servos_pose)
+		# self.x_motor.set_angle(msg.x)
+		# self.y_motor.set_angle(msg.y)
 
 def main():
 	rclpy.init()
--- jetson/ros2_ws/src/fobo/scripts/move_control.py
+++ jetson/ros2_ws/src/fobo/scripts/move_control.py
@@ -4,59 +4,33 @@ from rclpy.node import Node
 import Jetson.GPIO as GPIO
 
 from fobo.msg import Velocity
-
-
-class Motor():
-	def __init__(self, pin, direction):
-		GPIO.setup(pin, GPIO.OUT)
-		self.pwm = GPIO.PWM(pin, 490)
-		self.dir = direction
-		self.dc = 0
-		GPIO.setup(self.dir, GPIO.OUT, initial=GPIO.HIGH)
-		GPIO.output(self.dir, GPIO.HIGH)
-		self.pwm.start(self.dc)
-
-	def __del__(self):
-		self.pwm.stop()
-
-	def set_speed(self, dc):
-		self.dc = dc
-		if self.dc < 0:
-			GPIO.output(self.dir, GPIO.LOW)
-			self.dc = -self.dc
-		else:
-			GPIO.output(self.dir, GPIO.HIGH)
-		if self.dc > 100:
-			self.dc = 100
-		self.pwm.ChangeDutyCycle(self.dc)
+from fobo.msg import ServosPose
 
 
 class FoboMovement(Node):
 	def __init__(self):
 		super().__init__('FoboMovement')
-		GPIO.setmode(GPIO.BOARD)
-		self.left_motor = Motor(33, 31)
-		self.right_motor = Motor(32, 35)
-		self.velocity = {'linear': 0, 'angular': 0}
-		self.sub = self.create_subscription(
+		self.velocity = Velocity()
+		self.pub = self.create_publisher(
 			Velocity,
 			'velocity',
-			self.read_velocity,
+			10
+		)
+		self.sub = self.create_subscription(
+			ServosPose,
+			"servos_pose",
+			self.read_servos_pose,
 			10
 		)
 		self.sub
-		self.L = 0.2
-		self.r = 0.1
 
-	def read_velocity(self, msg):
-		self.velocity['linear'] = msg.linear
-		self.velocity['angular'] = msg.angular
-		left_wheel_speed = self.velocity['linear'] - ((self.L/2) * self.velocity['angular']) / self.r
-		right_wheel_speed = self.velocity['linear'] + ((self.L/2) * self.velocity['angular']) / self.r
-		self.left_motor.set_speed(left_wheel_speed)
-		self.right_motor.set_speed(right_wheel_speed)
-		print(f"left: {left_wheel_speed} right: {right_wheel_speed}")
-
+	def read_servos_pose(self, msg):
+		self.velocity.linear = 50
+		if msg.servo_x > 90:
+			self.velocity.angular = int((msg.servo_x - 90) * 0.9) # vel = pose * 100 % / 90º			pose goes from 0 to 90
+		elif msg.servo_x < 90:
+			self.velocity.angular = int((msg.servo_x - 90) * 0.9) # vel = -pose * 100 % / 90º			pose goes from 0 to -90
+		self.pub.publish(self.velocity)
 
 def main():
 	rclpy.init()
